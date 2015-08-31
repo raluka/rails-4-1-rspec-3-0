d47b655d96d166e32c302d024dd288f0f91d94a6
raluka <badoi.raluca@gmail.com>
2015-08-31 16:31:12 +0200

    created test requirements for contact model

---
+++ spec/models/contact_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+describe Contact do
+
+  it 'is valid with firstname, lastname and email'
+  it 'is invalid without a firstname'
+  it 'is invalid without a lastname'
+  it 'is invalid without an email adress'
+  it "returns a contact's full name as a string"
+
+end
